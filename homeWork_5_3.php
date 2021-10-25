dc8cd09182eb34164a4b01e702c2e5e9d8c81b5b
usernamekutsarvasyagit config --global user.name usernamekutsarvasya <vkutsar89@gmail.com>
2021-10-25 20:18:35 +0300

    ad

--- homeWork_5_3.php
+++ homeWork_5_3.php
@@ -1,32 +1,30 @@
 <?php
 
-$array = file('FizzBuzz.txt');
+file_put_contents('newFizzBuzz.txt', '');
 
-array_map('fizz', $array);
+$array_of_rows = file('FizzBuzz.txt');
 
-function fizz($a)
+array_map('fizzBuzz', $array_of_rows);
+
+function fizzBuzz($one_row)
 {
-    $exp = explode(" ", $a);
-    $array = range(1, $exp[2]);
-    $string = array_map(function ($a) use ($exp) {
-        $str = '';
-        if ($a % $exp['0'] == 0 && $a % $exp['1'] == 0) {
-            $str .= 'fizzbuzz';
-        } elseif ($a % $exp['0'] == 0) {
-            $str .= 'f';
-        } elseif ($a % $exp[1] == 0) {
-            $str .= 'b';
+    $array_data_for_fizz_buzz = explode(" ", $one_row);
+    $array_of_numbers = range(1, $array_data_for_fizz_buzz[2]);
+    $fizz = $array_data_for_fizz_buzz[0];
+    $buzz = $array_data_for_fizz_buzz[1];
+    $resul_array_of_map_function = array_map(function ($number) use ($fizz, $buzz) {
+        $string = '';
+        if ($number % $fizz == 0 && $number % $buzz == 0) {
+            $string .= 'fizzbuzz';
+        } elseif ($number % $fizz == 0) {
+            $string .= 'f';
+        } elseif ($number % $buzz == 0) {
+            $string .= 'b';
         } else {
-            $str .= $a;
+            $string .= $number;
         }
-        return $str;
-    }, $array);
-    $string = implode('',$string);
-    file_put_contents('newFizzBuzz.txt', $string . "\n", FILE_APPEND);
+        return $string;
+    }, $array_of_numbers);
+    $string_of_one_row = implode('', $resul_array_of_map_function);
+    file_put_contents('newFizzBuzz.txt', $string_of_one_row . "\n", FILE_APPEND);
 }
-
-
-
-
-
-

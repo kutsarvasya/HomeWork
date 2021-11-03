7f8206e8e0b8e237d11a7ec36ea574c1a98b1807
usernamekutsarvasyagit config --global user.name usernamekutsarvasya <vkutsar89@gmail.com>
2021-11-03 18:28:27 +0200

    12

--- homeWork_8_2.php
+++ homeWork_8_2.php
@@ -1,1 +1,18 @@
 <?php
+$a=$argv[1];
+$b='';
+if ($a%2!=0) {
+    for ($i = 1, $k = $a; $i < $a; $i++, $k--) {
+        if ($k % 2 != 0) {
+            $b .= str_repeat(" ", intdiv($k, 2)) . str_repeat('*', $i) . "\n";
+        }
+    }
+
+    for ($i = 1, $k = $a; $i <= $a; $i++, $k--) {
+        if ($k % 2 != 0) {
+            $b .= str_repeat(" ", intdiv($i, 2)) . str_repeat('*', $k) . "\n";
+        }
+    }
+
+    echo $b;
+}

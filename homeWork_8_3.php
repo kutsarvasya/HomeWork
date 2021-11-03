52b675484caac6f404c378e4da232ad61206f968
usernamekutsarvasyagit config --global user.name usernamekutsarvasya <vkutsar89@gmail.com>
2021-11-03 20:16:08 +0200

    ad

--- homeWork_8_3.php
+++ homeWork_8_3.php
@@ -1,1 +1,19 @@
 <?php
+$numbers= file($argv[1]);
+
+
+
+
+
+foreach ($numbers as $number) {
+    list($a, $b) = explode(';', $number);
+    $a = explode(' ', trim($a));
+    $b = explode(' ', trim($b));
+
+
+    if (intdiv(array_sum($a), count($a)) == $b[0] && array_sum($a)%count($a)==$b[1]) {
+        echo 'true' . "\n";
+    }else  echo 'false' . "\n";
+
+}
+
